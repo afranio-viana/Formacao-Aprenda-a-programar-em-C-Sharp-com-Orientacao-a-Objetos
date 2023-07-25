948c9aa33af10245f8cd12f780fa4976731ad8df
Afranio Viana <18465@bemol.local>
2023-07-25 10:12:47 -0400

    Aplicando conceitos de public, internal e static

---
+++ C# - dominando Orientação a Objetos/ScreenSound/Modelos/Avaliacao.cs
@@ -0,0 +1,22 @@
+namespace ScreenSound.Modelos;
+
+internal class Avaliacao
+{
+    public Avaliacao(int nota)
+    {
+        Nota = nota;
+    }
+
+    public int Nota { get;}
+    public static Avaliacao Parse(string texto){
+        int nota = int.Parse(texto);
+        if (nota<0)
+        {
+            nota = 0;
+        }else if (nota >10)
+        {
+            nota = 10;
+        }
+        return new Avaliacao(nota);
+    }
+}
