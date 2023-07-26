990a9a857f696d1b335e2288eaacea5e0f72d5c7
Afranio Viana <18465@bemol.local>
2023-07-26 10:38:18 -0400

    Aplicando try/catch e integrando API's

---
+++ C# - consumindo API, gravando arquivos e utilizando o LINQ/ScreenSound-04/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string respost = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        Console.WriteLine(respost);
+    }catch (Exception ex)
+    {
+        Console.WriteLine($"\n\nInfelizmente não funfou!!!\n\nMotivo: {ex.Message}");
+    }
+}
