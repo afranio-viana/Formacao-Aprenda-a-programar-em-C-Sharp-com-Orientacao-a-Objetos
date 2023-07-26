e3277d9d5d75ec32bc48a736320c9822e9991e96
Afranio Viana <18465@bemol.local>
2023-07-26 11:20:16 -0400

    Aplicando serialização de desserialização

--- C# - consumindo API, gravando arquivos e utilizando o LINQ/ScreenSound-04/Program.cs
+++ C# - consumindo API, gravando arquivos e utilizando o LINQ/ScreenSound-04/Program.cs
@@ -1,10 +1,17 @@
-﻿Console.Clear();
+﻿using ScreenSound_04.Modelos;
+using System.Text.Json;
+
+Console.Clear();
 using (HttpClient client = new HttpClient())
 {
     try
     {
-        string respost = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
-        Console.WriteLine(respost);
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+        foreach (Musica musica in musicas.OrderBy(mu => mu.Artista))
+        {
+            musica.ExibirDetalhesDaMusica();
+        }
     }catch (Exception ex)
     {
         Console.WriteLine($"\n\nInfelizmente não funfou!!!\n\nMotivo: {ex.Message}");
